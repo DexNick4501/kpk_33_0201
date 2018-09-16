2572c41d4f6f3717b6028c84eaa419c5a9c54805
DexNick4501 <idesignerdisk2@yandex.ru>
2018-09-16 20:58:37 +0500

    Added a new group to the "step_2.py" at number 13, and filled it new students
    ***********
    
    16.09.2018

--- college/step_2.py
+++ college/step_2.py
@@ -4,6 +4,12 @@ from college import Student, Group, Teacher
 # [print(el) for el in students]
 
 print('*' * 25)
+students_13 = Student.import_from_file('13_group.src')
+group_13 = Group('13', 'Прикладная информатика 09.02.07')
+group_13.add_students(students_13)
+[print(el, el.group) for el in students_13]
+
+print('*' * 25)
 students_23 = Student.import_from_file('23_group.src')
 group_23 = Group('23', 'Прикладная информатика 09.02.05')
 group_23.add_students(students_23)
@@ -14,6 +20,7 @@ students_33 = Student.import_from_file('33_group.src')
 group_33 = Group('33', 'Прикладная информатика 09.02.05')
 group_33.add_students(students_33)
 [print(el, el.group) for el in students_33]
+
 
 print('*' * 25)
 teachers = Teacher.import_from_file('teachers.src')
