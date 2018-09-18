c39b3ae68744da31eb20607247bd8bfb374f3cc5
Student <roma.kosarev20@yandex.ru>
2018-09-18 12:25:27 +0600

    add new group "Pizzeria"

--- college/step_2.py
+++ college/step_2.py
@@ -3,11 +3,11 @@ from college import Student, Group, Teacher
 # students = Student.import_from_file('students.src')
 # [print(el) for el in students]
 
-print('*' * 25)
-students_13 = Student.import_from_file('13_group.src')
-group_13 = Group('13', 'Прикладная информатика 09.02.07')
-group_13.add_students(students_13)
-[print(el, el.group) for el in students_13]
+# print('*' * 25)
+# students_13 = Student.import_from_file('13_group.src')
+# group_13 = Group('13', 'Прикладная информатика 09.02.07')
+# group_13.add_students(students_13)
+# [print(el, el.group) for el in students_13]
 
 print('*' * 25)
 students_23 = Student.import_from_file('23_group.src')
